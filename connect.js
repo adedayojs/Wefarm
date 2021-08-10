f69793113292121f3e282c83b654b4d972f80f78
adedayojs <samfeolu@gmail.com>
2021-08-10 15:32:11 +0100

    Update Validation to not make usr of alert

--- connect.js
+++ connect.js
@@ -1,32 +1,32 @@
 function attachEventListner() {
   const form = document.querySelector("form");
   form.addEventListener("submit", (e) => {
+    console.log("county Prevent");
     e.preventDefault();
-    onSubmitPhoneForm();
+    onSubmitCountyForm();
   });
-  
 }
 
-function onSubmitPhoneForm() {
-  const phone = document.getElementById("phonenumber");
-  if (!phone.value) {
+function onSubmitCountyForm() {
+  const county = document.getElementById("county");
+  if (!county.value) {
     // Value is not Present. Ideally this should never pass because the html required attribute is present, however never trust user input to check!
-    displyError("Please Input a Phone number");
+    displyError("Please select a County", "county-error");
     return;
   }
-  const phoneRegex =
-    /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/;
-  if (!phoneRegex.test(phone.value)) {
-    displyError("Please Input Valid Phone number");
-    return;
-  }
-  window.location.href = "connect.html";
+  alert("Success");
 }
 
-function onSubmitCountyForm() {}
+function displyError(err, elementId) {
+  const errorLabel = document.getElementById(elementId);
+  errorLabel.innerText = err;
+  errorLabel.style = "visibility: visible";
 
-function displyError(err) {
-  alert(err);
+  setTimeout(() => {
+    errorLabel.style = "visibility: hidden";
+  }, 1500);
+
+  // alert(err);
 }
 
 attachEventListner();
