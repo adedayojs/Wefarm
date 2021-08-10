43f0a2c58fd7b94892bb336ee18164163c4764f1
adedayojs <samfeolu@gmail.com>
2021-08-10 15:00:09 +0100

    Create methods to handle error management and form listeing

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function submitForm(event) {
-  event.preventDefault();
-  console.log(event);
-  console.log(this);
+function attachEventListner() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+  });
+  console.log("her");
+}
+
+attachEventListner();
+function onSubmit() {
+    const phone = document.getElementById('phonenumber')
+    if(phone.value){
+        // Value is Present
+    }
+}
+
+function displyError(){
+    
 }
