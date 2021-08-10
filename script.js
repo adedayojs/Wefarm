525b365c2f85bd983fd5cadf47c16a1c8bbf1145
adedayojs <samfeolu@gmail.com>
2021-08-10 15:08:58 +0100

    Check for validation

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ function attachEventListner() {
 attachEventListner();
 function onSubmit() {
   const phone = document.getElementById("phonenumber");
-  if (phone.value) {
-    // Value is Present
+  if (!phone.value) {
+    // Value is not Present. Ideally this should never pass because the html required attribute is present, however never trust user input to check!
+    displyError("Please Input a Phone number");
+    return;
   }
   const phoneRegex =
     /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/;
